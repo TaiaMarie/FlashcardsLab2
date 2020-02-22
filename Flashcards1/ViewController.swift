15b1a031c94f15c6a502572746a4b418142ca135
Taia Williams-Rivera <tmwilliams181@students.desu.edu>
2020-02-22 17:01:56 -0500

    Initial Commit

---
+++ Flashcards1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards1
+//
+//  Created by Taia Williams on 2/22/20.
+//  Copyright © 2020 Taia Williams-Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
